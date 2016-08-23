424aa0cd7c56ef627abc7c2133056c0db72420ff
James Fysh <james.fysh@gmail.com>
2016-08-24 05:43:15 +1000

    Remove reliance on shapely
    QgsGeometry provides essentially the same functionality as the various shapely
    geometry types (hint: they both wrap geos!).  Now re-written so that there are
    no external dependencies.
    
    Unit-tests needed ever-so-slight tweaking, hopefully this hasn't broken the
    underlying functionality of the plugin (I've done limited testing of the
    plugin itself -- more to come).

--- EleView/ElevationReader.py
+++ EleView/ElevationReader.py
@@ -1,27 +1,29 @@
 from PyQt4.QtCore import QPointF
 from qgis.core import (
     QgsMessageLog, QgsRectangle, QgsFeatureRequest, QgsGeometry,
-    QgsCoordinateTransform, QgsPoint
+    QgsCoordinateTransform, QgsPoint, QGis
 )
-
-from shapely.geometry import LineString, MultiPoint, MultiLineString, Polygon
-from shapely import wkt
 
 
 def de_polygonize(geometry):
-    if isinstance(geometry, Polygon):
-        coords = geometry.exterior.coords
-        return MultiLineString([
-            LineString([p1, p2])
-            for p1, p2 in zip(coords[0::1], coords[1::1])
-        ])
-    return geometry
+    if geometry.type() == QGis.Polygon:
+        polygon = geometry.asPolygon()
+        exterior = polygon[0]
+        result = [
+            QgsGeometry.fromPolyline([p1, p2])
+            for p1, p2 in zip(exterior[0::1], exterior[1::1])
+        ]
+        print "results:"
+        for l in result:
+            print l.exportToWkt()
+        return result
+    return [geometry]
 
 
 def flatten(pts):
-    if isinstance(pts, MultiPoint):
-        return list(pts.geoms)
-    return [pts]
+    if pts.type() == QGis.Point and pts.isMultipart():
+        return list(pts.asMultiPoint())
+    return [pts.asPoint()]
 
 
 class ElevationReader(object):
@@ -63,21 +65,23 @@ class ElevationReader(object):
         for a_geometry in some_geometries:
             a_geometry.transform(self.xform)
 
-    def _generate_points(self, line, length, feature_map):
+    def _generate_points(self, line, feature_map):
         points = []
-        for feat, geom in feature_map.items():
-            if not geom.intersects(line):
-                continue
-            elevation = float(feat.attribute(self.layer_attr))
-            for x_value in [
-                length * line.project(pt, True)
-                for pt in flatten(line.intersection(geom))
-            ]:
-                points.append(QPointF(x_value, elevation))
+        print "over", line.exportToWkt()
+        for feat, geoms in feature_map.items():
+            for geom in geoms:
+                if not geom.intersects(line):
+                    continue
+                elevation = float(feat.attribute(self.layer_attr))
+                for x_value in [
+                    pt.x()
+                    for pt in flatten(line.intersection(geom))
+                ]:
+                    points.append(QPointF(x_value, elevation))
         if points:
             min_px = min(p.x() for p in points)
             if min_px < 0.:
-                points = [QPointF(p.x() + (-min_px), p.y()) for p in points]
+                points = [QPointF(p.x() + abs(min_px), p.y()) for p in points]
             points = sorted(points, key=lambda p: p.x())
         self.points = points
 
@@ -87,10 +91,9 @@ class ElevationReader(object):
         xfpt1, xfpt2 = self.xform.transform(QgsPoint(self.pt1)), \
                        self.xform.transform(QgsPoint(self.pt2))
         self._generate_points(
-            LineString([xfpt1, xfpt2]),
-            xfpt1.distance(xfpt2),
+            QgsGeometry.fromPolyline([xfpt1, xfpt2]),
             {
-                f: de_polygonize(wkt.loads(g.exportToWkt()))
+                f: de_polygonize(g)
                 for f, g in feat_geom_map.items()
             }
         )
