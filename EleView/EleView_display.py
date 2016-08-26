c3c68bfcdecac3a1635c42fd736585d5550e3c00
James Fysh <james.fysh@gmail.com>
2016-08-27 08:07:46 +1000

    Start splitting the QGraphicsView stuff out
    The EleView_display script should be responsible for the dialog + signals &
    slots, while ElevationScene takes care of constructing the scene and updating
    it when required.

--- EleView/EleView_display.py
+++ EleView/EleView_display.py
@@ -1,23 +1,12 @@
-from math import sqrt
 from functools import partial
 
 from PyQt4.QtCore import Qt, QPointF, QLineF, QObject, SIGNAL
-from PyQt4.QtGui import (
-    QPainterPath, QGraphicsScene, QPen, QBrush, QColor, QLinearGradient
-)
 
 from qgis.core import QgsMessageLog
 
 from .ElevationReader import ElevationReader
+from .ElevationScene import ElevationScene, PT1, PT2
 from .EleView_dialogs import EleViewDialogDisp
-
-ZOOM_IN_FACTOR = 1.25
-ZOOM_OUT_FACTOR = 1 / ZOOM_IN_FACTOR
-
-
-def fresnel_radius(distance_meters, frequency_mhz):
-    radius_meters = 17.32 * sqrt(distance_meters / (4 * frequency_mhz))
-    return radius_meters
 
 
 class ElevationDisplay(object):
@@ -32,8 +21,7 @@ class ElevationDisplay(object):
         self.orig_pt1_y = None
         self.orig_pt2_y = None
         self.display = None
-        self.line = None
-        self.path = None
+        self.scene = None
 
         pt1, pt2 = ptsCaptured
         self.reader = ElevationReader(
@@ -52,11 +40,12 @@ class ElevationDisplay(object):
         # And display
         self.show_elevation(self.reader.points)
 
-    def configure_display(self):
+    def configure_dialog(self):
         # Helper method to set up the elevation-view dialog
         if self.display is None:
             self.display = EleViewDialogDisp()
             self.display.wheelEvent = self.wheel_event
+            self.scene = ElevationScene(self.display, self.display.eleView)
         for slider in (self.display.pt1Slider, self.display.pt2Slider):
             QObject.connect(
                 slider,
@@ -66,59 +55,46 @@ class ElevationDisplay(object):
         QObject.connect(
             self.display.enableFresnel,
             SIGNAL("stateChanged(int)"),
-            self.display.frequency.setEnabled
+            self.fresnel_event
+        )
+        QObject.connect(
+            self.display.frequency,
+            SIGNAL("valueChanged(int"),
+            self.freq_event
         )
 
     def hide(self):
         # Hide the elevation-view dialog
         if self.display:
             self.display.hide()
+        self.scene = None
         self.display = None
 
     def wheel_event(self, event):
-        if self.display is None:
+        if self.scene is None:
             return
 
-        qgv = self.display.eleView
-
-        # Save the scene pos
-        old_pos = qgv.mapToScene(event.pos())
-
-        # Zoom
-        zoom_factor = ZOOM_IN_FACTOR if event.delta() > 0 else ZOOM_OUT_FACTOR
-        qgv.scale(zoom_factor, zoom_factor)
-
-        # Get the new position
-        new_pos = qgv.mapToScene(event.pos())
-
-        # Move scene to old position
-        delta = new_pos - old_pos
-        qgv.translate(delta.x(), delta.y())
+        self.scene.zoom_event(event.pos(), event.delta())
 
     def slider_moved(self, slider, value):
-        if self.display is None:
+        if self.scene is None:
             return
+        self.scene.slider_event(
+            {self.display.pt1Slider: PT1, self.display.pt2Slider: PT2}[slider],
+            value
+        )
 
-        # Copy the current line
-        line = QLineF(self.line.line())
-        
-        # Mutate it based on the value provided by the slider
-        method, line_pt, orig_pt_y, lbl = {
-            self.display.pt1Slider: (line.setP1, line.p1(), self.orig_pt1_y, self.display.lblPt1),
-            self.display.pt2Slider: (line.setP2, line.p2(), self.orig_pt2_y, self.display.lblPt2),
-        }[slider]
-        method(QPointF(line_pt.x(), orig_pt_y + value))
+    def freq_event(self, value):
+        if self.scene is None:
+            return
+        self.scene.fresnel_event(self.frequency.enabled, value)
 
-        # Colour the line as appropriate
-        self.line.setPen(QPen(
-            Qt.red if self.line.collidesWithItem(self.path) else Qt.green, 1
-        ))
-        
-        # And update the line geometry - this shall be reflected in the scene
-        self.line.setLine(line)
-
-        # Update the relevant label for the slider
-        lbl.setText("+{} m".format(value))
+    def fresnel_event(self, state):
+        enabled = True if state else False
+        frequency = self.display.frequency
+        frequency.setEnabled(enabled)
+        if self.scene:
+            self.scene.fresnel_event(enabled, frequency.value)
 
     @staticmethod
     def log_points(the_points):
@@ -135,42 +111,8 @@ class ElevationDisplay(object):
     def show_elevation(self, points):
         if not points:
             return
-        self.configure_display()
-
         self.log_points(points)
-
-        # Set up the scene
-        path = QPainterPath(points[0])
-        for point in points[1:]:
-            path.lineTo(point)
-        path.lineTo(points[-1].x(), 0)
-        path.lineTo(points[0].x(), 0)
-        path.lineTo(points[0])
-
-        height = path.boundingRect().height()
-
-        scene = QGraphicsScene(self.display)
-        gradient = QLinearGradient(QPointF(0., height), QPointF(0., 0.))
-        gradient.setColorAt(0, QColor(0x00b300))
-        gradient.setColorAt(1, QColor(0x331a00))
-        self.path = scene.addPath(
-            path,
-            QPen(Qt.blue, 1),
-            QBrush(gradient)
-        )
-        self.line = scene.addLine(
-            QLineF(points[0], points[-1]),
-            QPen(Qt.green, 1)
-        )
-        self.orig_pt1_y = points[0].y()
-        self.orig_pt2_y = points[-1].y()
-
-        if self.line.collidesWithItem(self.path):
-            self.line.setPen(QPen(Qt.red, 1))
-
-        # Set up the view
-        view = self.display.eleView
-        view.setScene(scene)
-        view.ensureVisible(scene.sceneRect())
-        view.scale(1., -1.)
+        self.configure_dialog()
+        frequency = self.display.frequency
+        self.scene.initialize(points, frequency.enabled, frequency.value)
         self.display.show()
---
+++ EleView/ElevationScene.py
@@ -0,0 +1,117 @@
+from math import sqrt, atan2
+
+from PyQt4.QtGui import (
+    QPainterPath, QGraphicsScene, QPen, QBrush, QColor, QLinearGradient
+)
+from PyQt4.QtCore import Qt, QPointF, QLineF
+
+ZOOM_IN_FACTOR = 1.25
+ZOOM_OUT_FACTOR = 1 / ZOOM_IN_FACTOR
+PT1, PT2 = "first point", "second point"
+
+
+def fresnel_radius(distance_meters, frequency_mhz):
+    radius_meters = 17.32 * sqrt(distance_meters / (4 * frequency_mhz))
+    return radius_meters
+
+
+class ElevationScene(object):
+    def __init__(self, dialog, view):
+        self.dialog = dialog
+        self.view = view
+        self.line = None
+        self.path = None
+        self.zone = None
+        self.p_y = {}
+
+    @staticmethod
+    def path_for(points):
+        path = QPainterPath(points[0])
+        for point in points[1:]:
+            path.lineTo(point)
+        path.lineTo(points[-1].x(), 0)
+        path.lineTo(points[0].x(), 0)
+        path.lineTo(points[0])
+        return path
+
+    @staticmethod
+    def overlay_for(pt1, pt2, frequency):
+        line = QLineF(pt1, pt2)
+        radius = fresnel_radius(pt2.distance(pt1), frequency)
+        zone = None
+
+        return line, zone
+
+    def initialize(self, points, show_fresnel, frequency):
+        self.p_y = {
+            PT1: points[0].y(),
+            PT2: points[-1].y()
+        }
+
+        # Construct geometries to be included in the scene
+        path = self.path_for(points)
+        line, zone = self.overlay_for(points[0], points[-1], frequency)
+
+        height = path.boundingRect().height()
+        gradient = QLinearGradient(QPointF(0., height), QPointF(0., 0.))
+        gradient.setColorAt(0, QColor(0x00b300))
+        gradient.setColorAt(1, QColor(0x331a00))
+
+        # Create the scene
+        scene = QGraphicsScene(self.dialog)
+
+        # Add geometries to the scene, keeping a reference to each
+        self.path = scene.addPath(path, QPen(Qt.blue, 1), QBrush(gradient))
+        self.line = scene.addLine(line, QPen(Qt.green, 1))
+        self.zone = scene.addEllipse(0., 0., 0., 0.)
+
+        if self.line.collidesWithItem(self.path):
+            self.line.setPen(QPen(Qt.red, 1))
+
+        # Set up the view
+        self.view.setScene(scene)
+        self.view.ensureVisible(scene.sceneRect())
+        self.view.scale(1., -1.)
+
+    def slider_event(self, point, value):
+        # Copy the current line
+        line = QLineF(self.line.line())
+
+        # Mutate it based on the value provided by the slider
+        method, line_pt, lbl = {
+            PT1: (line.setP1, line.p1(), self.display.lblPt1),
+            PT2: (line.setP2, line.p2(), self.display.lblPt2),
+        }[point]
+        method(QPointF(line_pt.x(), self.p_y[point] + value))
+
+        # And update the line geometry - this shall be reflected in the scene
+        self.line.setLine(line)
+
+        # Colour the line as appropriate
+        self.line.setPen(QPen(
+            Qt.red if self.line.collidesWithItem(self.path) else Qt.green, 1
+        ))
+
+        # Update the relevant label for the slider
+        lbl.setText("+{} m".format(value))
+
+    def zoom_event(self, pos, delta):
+        # Save the scene pos
+        old_pos = self.view.mapToScene(pos)
+
+        # Zoom
+        zoom_factor = ZOOM_IN_FACTOR if delta > 0 else ZOOM_OUT_FACTOR
+        self.view.scale(zoom_factor, zoom_factor)
+
+        # Get the new position
+        new_pos = self.view.mapToScene(pos)
+
+        # Move scene to old position
+        delta = new_pos - old_pos
+        self.view.translate(delta.x(), delta.y())
+
+    def fresnel_event(self, show_fresnel, value):
+        if show_fresnel is False:
+            self.zone.hide()
+        else:
+            pass
